86ba6c05afcbc03cd5baf9cbe8f70011e28e8c4a
Dionysis <dionysiskatsetis@gmail.com>
2026-04-08 19:15:28 +0300

    fixed files

--- utils.c
+++ utils.c
@@ -8,3 +8,29 @@ void check(int res, const char* msg){
         _exit(0);
     }
 }
+
+int safe_atoi(char *s, int *val)
+{
+	long l;
+	char *endp;
+
+	l = strtol(s, &endp, 10);
+	if (s != endp && *endp == '\0') { // check if at least one digit was read and if the whole string converted to a number 
+		*val = l;
+		return 0;
+	} else
+		return -1;
+}
+
+void *safe_malloc(size_t size)
+{
+	void *p;
+
+	if ((p = malloc(size)) == NULL) {
+		fprintf(stderr, "Out of memory, failed to allocate %zd bytes\n",
+			size);
+		exit(1);
+	}
+
+	return p;
+}
--- utils.h
+++ utils.h
@@ -2,3 +2,6 @@
 
 void check(int res,const char* msg);
 
+int safe_atoi(char *s, int *val);
+
+void *safe_malloc(size_t size);
